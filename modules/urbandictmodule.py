1dba1f159cd731eb084ee3002c0c8928ad7366e6
deepfriedcyanide <erik.geiser@yahoo.de>
2015-09-26 00:55:23 +0200

    Urbandictmodule is now a class
    * Rewrote the urbandictmodule to fit with the object oriented style

--- modules/urbandictmodule.py
+++ modules/urbandictmodule.py
@@ -1,35 +1,44 @@
+"""
+    Urban Dictionary Module
+"""
+
 import urbandict
 
-def check(client,message):
-    msg = message.content
+class Murbandict():
 
-    if msg.startswith("!def "):
-        print("Urbandictionary module called!")
-        query = msg.replace("!def ","")
-        query = query.split()
-        try:
-            if len(query)>1:
-                number = int(query[len(query)-1])
-                if number > 5:
-                    number = 5
-                del query[len(query)-1]
-            else:
+    def __init__(self,dclient):
+        self.dclient = dclient
+
+    def check(self,message):
+        msg = message.content
+
+        if msg.startswith("!def "):
+            print("Urbandictionary module called!")
+            query = msg.replace("!def ","")
+            query = query.split()
+            try:
+                if len(query)>1:
+                    number = int(query[len(query)-1])
+                    if number > 5:
+                        number = 5
+                    del query[len(query)-1]
+                else:
+                    number = 1
+            except:
+                print ("Could not convert to int")
                 number = 1
-        except:
-            print ("Could not convert to int")
-            number = 1
 
-        query = " ".join(query)
+            query = " ".join(query)
 
-        result = urbandict.define(query)
-        if number>len(result):
-            number=len(result)
+            result = urbandict.define(query)
+            if number>len(result):
+                number=len(result)
 
-        for i in range(number):
-            title = " ".join(result[i]['word'].replace("\n","").split())
-            definition = " ".join(result[i]['def'].replace("\n","").split())
-            example = " ".join(result[i]['example'].replace("\n","").split())
+            for i in range(number):
+                title = " ".join(result[i]['word'].replace("\n","").split())
+                definition = " ".join(result[i]['def'].replace("\n","").split())
+                example = " ".join(result[i]['example'].replace("\n","").split())
 
-            client.send_message(message.channel,"**"+title+"**")
-            client.send_message(message.channel, definition)
-            client.send_message(message.channel,"*"+example+"*")
+                self.dclient.send_message(message.channel,"**"+title+"**")
+                self.dclient.send_message(message.channel, definition)
+                self.dclient.send_message(message.channel,"*"+example+"*")
