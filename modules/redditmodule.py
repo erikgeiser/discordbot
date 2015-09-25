36246cb7581f81eb22ed55a46f679b4c36e36653
Thilo Birkenfeld <thilo.birkenfeld@web.de>
2015-09-26 00:42:31 +0200

    Redditmodule is now a class
    * Rewrote the redditmodule to fit with the object oriented style

--- modules/redditmodule.py
+++ modules/redditmodule.py
@@ -1,25 +1,37 @@
 """
     Reddit Module of the Discord Bot
 """
+import praw
 
-def getredditnews(client,reddit,channel,subreddit):
+class Mreddit:
 
-    newslist = reddit.get_subreddit(subreddit).get_hot(limit=5)
+    def __init__(self,dclient,user_agent):
+        try:
+            self.dclient = dclient
+            self.user_agent = user_agent
+            self.rclient = praw.Reddit(user_agent=user_agent)
+            self.failed = False
+        except:
+            self.failed = True
 
-    for news in newslist:
-        client.send_message(channel, '**'+news.title+'** > '+news.short_link)
-        if len(news.selftext)>250:
-            client.send_message(channel,news.selftext[:250].replace("*","\*")+"...")
-        elif news.selftext!='':
-            client.send_message(channel,news.selftext[:len(news.selftext)].replace("*","\*"))
+    def getredditnews(self,channel,subreddit):
+
+        newslist = self.rclient.get_subreddit(subreddit).get_hot(limit=5)
+
+        for news in newslist:
+            self.dclient.send_message(channel, '**'+news.title+'** > '+news.short_link)
+            if len(news.selftext)>250:
+                self.dclient.send_message(channel,news.selftext[:250].replace("*","\*")+"...")
+            elif news.selftext!='':
+                self.dclient.send_message(channel,news.selftext[:len(news.selftext)].replace("*","\*"))
 
 
-def check(client,reddit,message):
-    msg = message.content
-    if msg.startswith("!r "):
-        sub = msg.replace("!r ","")
-        getredditnews(client, reddit, message.channel, sub)
-    elif msg=="!csgo":
-        getredditnews(client, reddit, message.channel, "globaloffensive")
-    elif msg=="!tfts":
-        getredditnews(client, reddit, message.channel, "talesfromtechsupport")
+    def check(self,message):
+        msg = message.content
+        if msg.startswith("!r "):
+            sub = msg.replace("!r ","")
+            self.getredditnews(message.channel, sub)
+        elif msg=="!csgo":
+            self.getredditnews(message.channel, "globaloffensive")
+        elif msg=="!tfts":
+            self.getredditnews(message.channel, "talesfromtechsupport")
